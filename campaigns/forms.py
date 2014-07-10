654c8983e1460caf02fb471872d9dee1640a65aa
Springsteen <martin.iv96@gmail.com>
2014-07-10 13:44:17 +0300

    Are required fields are added to student model, html pages are translated to bulgarian, the design of the html pages is edited.

--- campaigns/forms.py
+++ campaigns/forms.py
@@ -8,11 +8,9 @@ class CampaignForm(forms.models.ModelForm):
         fields = ('title', 'description')
         widgets = {
             'title': forms.fields.TextInput(attrs={
-                'placeholder': 'Enter camapaign title',
                 'class': '',
             }),
             'description': forms.fields.TextInput(attrs={
-                'placeholder': 'Enter camapaign description',
                 'class': '',
             }),
         }
@@ -22,24 +20,53 @@ class StudentForm(forms.models.ModelForm):
     class Meta:
         model = Student
         fields = (
-            'first_name', 'second_name',
-            'third_name', 'egn',
+            'first_name', 'second_name', 'third_name', 
+            'egn', 'address', 'parent_name',
+            'previous_school', 'bel_school', 'physics_school',
+            'bel_exam', 'maths_exam', 'maths_tues_exam',
+            'first_choice', 'second_choice',
         )
         widgets = {
             'first_name': forms.fields.TextInput(attrs={
-                'placeholder': 'Enter student first name',
                 'class': '',
             }),
             'second_name': forms.fields.TextInput(attrs={
-                'placeholder': 'Enter student second name',
                 'class': '',
             }),
             'third_name': forms.fields.TextInput(attrs={
-                'placeholder': 'Enter student third name',
                 'class': '',
             }),
             'egn': forms.fields.NumberInput(attrs={
-                'placeholder': 'Enter student egn',
+                'class': '',
+            }),
+            'address': forms.fields.TextInput(attrs={
+                'class': '',
+            }),
+            'parent_name': forms.fields.TextInput(attrs={
+                'class': '',
+            }),
+            'previous_school': forms.fields.TextInput(attrs={
+                'class': '',
+            }),
+            'bel_school': forms.fields.NumberInput(attrs={
+                'class': '',
+            }),
+            'physics_school': forms.fields.NumberInput(attrs={
+                'class': '',
+            }),
+            'bel_exam': forms.fields.NumberInput(attrs={
+                'class': '',
+            }),
+            'maths_exam': forms.fields.NumberInput(attrs={
+                'class': '',
+            }),
+            'maths_tues_exam': forms.fields.NumberInput(attrs={
+                'class': '',
+            }),
+            'first_choice': forms.fields.TextInput(attrs={
+                'class': '',
+            }),
+            'second_choice': forms.fields.TextInput(attrs={
                 'class': '',
             }),
         }
