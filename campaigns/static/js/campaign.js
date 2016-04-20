15c05ccc481c56cb7c53925aee48317d63bccde5
Martin Ivanov <martin.iv96@gmail.com>
2016-04-20 14:28:52 +0300

    Refactor JS files:  - Remove trailing spaces  - Convert tabs to spaces

--- campaigns/static/js/campaign.js
+++ campaigns/static/js/campaign.js
@@ -1,97 +1,97 @@
 $(document).ready(function (){
 
-	$("#show_campaign").prop('checked', true);
-	$("#show_campaign_halls").prop('checked', true);
+    $("#show_campaign").prop('checked', true);
+    $("#show_campaign_halls").prop('checked', true);
 
-	$(document).on("click", "#show_campaign", function(){
-		var checkValue = $(this);
-		if(checkValue.is(":checked")){
-			$("#show_campaign_table").show();
-		}else{
-			$("#show_campaign_table").hide();
-		}
-	});
+    $(document).on("click", "#show_campaign", function(){
+        var checkValue = $(this);
+        if(checkValue.is(":checked")){
+            $("#show_campaign_table").show();
+        }else{
+            $("#show_campaign_table").hide();
+        }
+    });
 
-	$(document).on("click", "#show_campaign_halls", function(){
-		var checkValue = $(this);
-		if(checkValue.is(":checked")){
-			$("#show_campaign_halls_table").show();
-		}else{
-			$("#show_campaign_halls_table").hide();
-		}
-	});
+    $(document).on("click", "#show_campaign_halls", function(){
+        var checkValue = $(this);
+        if(checkValue.is(":checked")){
+            $("#show_campaign_halls_table").show();
+        }else{
+            $("#show_campaign_halls_table").hide();
+        }
+    });
 
-	$(document).on("click", "#search_campaign_button", function(){
-		document.getElementById("search_campaign_button").disabled = true;
-		var searchUrl = document.URL;
-		searchUrl += "search";
+    $(document).on("click", "#search_campaign_button", function(){
+        document.getElementById("search_campaign_button").disabled = true;
+        var searchUrl = document.URL;
+        searchUrl += "search";
 
-		var $searchPanel = $("#campaign_search");
-		var $firstName = $("#search_first_name");
-		var $entryNumber = $("#search_entry_number");
+        var $searchPanel = $("#campaign_search");
+        var $firstName = $("#search_first_name");
+        var $entryNumber = $("#search_entry_number");
 
-		console.log("entry_number => ", $entryNumber.val(), "first_name => ", $firstName.val());
+        console.log("entry_number => ", $entryNumber.val(), "first_name => ", $firstName.val());
 
-		$.getJSON(
-			searchUrl,
-			{"first_name" : $firstName.val(), "entry_number" : $entryNumber.val()},
-			function(response){
-				var searchExists = document.getElementById("search_result");
-				if (searchExists == null){
-					$searchPanel.append("<div id=\"search_result\">");
-				}else{
-					$("#search_result").remove();
-					$searchPanel.append("<div id=\"search_result\">");
-				}
-				var $searchResultBox = $("#search_result");
+        $.getJSON(
+            searchUrl,
+            {"first_name" : $firstName.val(), "entry_number" : $entryNumber.val()},
+            function(response){
+                var searchExists = document.getElementById("search_result");
+                if (searchExists == null){
+                    $searchPanel.append("<div id=\"search_result\">");
+                }else{
+                    $("#search_result").remove();
+                    $searchPanel.append("<div id=\"search_result\">");
+                }
+                var $searchResultBox = $("#search_result");
 
-				if (response.hasOwnProperty('result_set')){
-					
-					var html = "<table class=\"table table-hover table-stripped\">";
+                if (response.hasOwnProperty('result_set')){
 
-					html += "<thead><tr>";
-					html += "<td><b>Собствено име</b></td>";
-					html += "<td><b>Фамилия</b></td>";
-					html += "<td><b>ЕГН</b></td>";
-					html += "<td></td>";
-					html += "<td></td>";
-					html += "</tr></thead>";
+                    var html = "<table class=\"table table-hover table-stripped\">";
 
-					html += "<tbody>";
-					for (id in response['result_set']){
-						html += "<tr>"
-						html += ("<td>" + response['result_set'][id]['first_name'] + "</td>");	
-						html += ("<td>" + response['result_set'][id]['third_name'] + "</td>");	
-						html += ("<td>" + response['result_set'][id]['egn'] + "</td>");	
-						html += ("<td><a href=\"/campaigns/" + response['campaign_id'] + "/students/" + response['result_set'][id]['id'] + "/\" >Още данни</a></td>");	
-						html += ("<td><a href=\"/campaigns/" + response['campaign_id'] + "/students/" + response['result_set'][id]['id'] + "/edit\" >Промени</a></td>");	
-						html += "</tr>";
-					}
+                    html += "<thead><tr>";
+                    html += "<td><b>Собствено име</b></td>";
+                    html += "<td><b>Фамилия</b></td>";
+                    html += "<td><b>ЕГН</b></td>";
+                    html += "<td></td>";
+                    html += "<td></td>";
+                    html += "</tr></thead>";
 
-					html += "</tbody></table>";
-					$searchResultBox.append(html);
+                    html += "<tbody>";
+                    for (id in response['result_set']){
+                        html += "<tr>"
+                        html += ("<td>" + response['result_set'][id]['first_name'] + "</td>");
+                        html += ("<td>" + response['result_set'][id]['third_name'] + "</td>");
+                        html += ("<td>" + response['result_set'][id]['egn'] + "</td>");
+                        html += ("<td><a href=\"/campaigns/" + response['campaign_id'] + "/students/" + response['result_set'][id]['id'] + "/\" >Още данни</a></td>");
+                        html += ("<td><a href=\"/campaigns/" + response['campaign_id'] + "/students/" + response['result_set'][id]['id'] + "/edit\" >Промени</a></td>");
+                        html += "</tr>";
+                    }
 
-				}else{
-					var warningExists = document.getElementById("search_warning");
-					if (warningExists == null){
-						$searchResultBox.append("<p id=\"search_warning\" class=\"alert alert-danger\">Не бяха намерени съвпадения.</p>");
-					}
-				}
-			}
-		);
+                    html += "</tbody></table>";
+                    $searchResultBox.append(html);
 
-		$firstName.val("");
-		$entryNumber.val("");
-	});
+                }else{
+                    var warningExists = document.getElementById("search_warning");
+                    if (warningExists == null){
+                        $searchResultBox.append("<p id=\"search_warning\" class=\"alert alert-danger\">Не бяха намерени съвпадения.</p>");
+                    }
+                }
+            }
+        );
 
-	$(document).on("click", "#clear_search_campaign_button", function(){
-		$("#search_result").remove();
-		document.getElementById("search_campaign_button").disabled = false;
-	});
+        $firstName.val("");
+        $entryNumber.val("");
+    });
 
-	$(document).on('click', '.delete-student', function (e) {
-		var answer = confirm('Сигурни ли сте, че искате да изтриете този кандидат?');
-		if (!answer) e.preventDefault();
-	})
+    $(document).on("click", "#clear_search_campaign_button", function(){
+        $("#search_result").remove();
+        document.getElementById("search_campaign_button").disabled = false;
+    });
+
+    $(document).on('click', '.delete-student', function (e) {
+        var answer = confirm('Сигурни ли сте, че искате да изтриете този кандидат?');
+        if (!answer) e.preventDefault();
+    })
 
 });
--- campaigns/static/js/form_validations.js
+++ campaigns/static/js/form_validations.js
@@ -1,123 +1,123 @@
 $(document).ready(function(){
-	
-	$(document).on("click", ".transaction_btn", function() {
-		$(this).prop("disabled", true);
-	});
 
-	/* CREATE STUDENT FORM */
+    $(document).on("click", ".transaction_btn", function() {
+        $(this).prop("disabled", true);
+    });
 
-	var createStudentButtonExists = document.getElementById("create_student_submit"); 	
+    /* CREATE STUDENT FORM */
 
-	if (createStudentButtonExists != null) {
-		createStudentButtonExists.disabled = true;
+    var createStudentButtonExists = document.getElementById("create_student_submit");
 
-		setInterval(
-			function(){
-				var firstNameLength = $("#id_first_name").val().length;
-				var secondNameLength = $("#id_second_name").val().length;
-				var thirdNameLength = $("#id_third_name").val().length;
-				var egnLength = $("#id_egn").val().length;
-				if ((firstNameLength > 0) && (secondNameLength > 0) &&
-					(thirdNameLength > 0) && (egnLength > 0)
-				){
-					createStudentButtonExists.disabled = false;
-				}else{
-					createStudentButtonExists.disabled = true;
-				}	
-			},
-			5000
-		);
-	}
+    if (createStudentButtonExists != null) {
+        createStudentButtonExists.disabled = true;
 
-	$(document).on("keyup", "#id_egn", function(){
-		var length = $(this).val().length;
-		var warningExists = document.getElementById("id_egn_warning");
-		var regexp = /(\d{10})/;
-		var match = regexp.exec($(this).val());
-		if (warningExists == null) {
-			
-				$("<div " 
-					+ "id=\"id_egn_warning\"" 
-					+ "class=\"alert alert-warning form_warning\">"
-					+ "</div>").insertAfter(this);
-			
-		}
-		if ((length < 10) || (match == null)) {
-			$("#id_egn_warning").removeClass("alert alert-success").addClass("alert alert-warning");
-			$("#id_egn_warning").text("ЕГН-то е твърде кратко или с неправилен формат");
-		}else{
-			$("#id_egn_warning").removeClass("alert alert-warning").addClass("alert alert-success");
-			$("#id_egn_warning").text("ЕГН-то е коректно");
-		}
-	});
+        setInterval(
+            function(){
+                var firstNameLength = $("#id_first_name").val().length;
+                var secondNameLength = $("#id_second_name").val().length;
+                var thirdNameLength = $("#id_third_name").val().length;
+                var egnLength = $("#id_egn").val().length;
+                if ((firstNameLength > 0) && (secondNameLength > 0) &&
+                    (thirdNameLength > 0) && (egnLength > 0)
+                ){
+                    createStudentButtonExists.disabled = false;
+                }else{
+                    createStudentButtonExists.disabled = true;
+                }
+            },
+            5000
+        );
+    }
 
-	$(document).on("keyup", ".student_grade_input", function(){
-		var gradeWritten = parseInt($(this).val()) || false;
-		var elemId = $(this).attr("id");
-		var warningExists = document.getElementById(elemId+"_warning");
-		if (warningExists == null) {
-			
-				$("<div " 
-					+ "id=\""+ elemId +"_warning\"" 
-					+ "class=\"alert alert-warning form_warning\">"
-					+ "</div>").insertAfter(this);
-			
-		}
-		if ((gradeWritten == false) || (gradeWritten < 2) || (gradeWritten > 6)) {
-			$("#" + elemId + "_warning").removeClass("alert alert-success").addClass("alert alert-warning");
-			$("#" + elemId + "_warning").text("Въведената оценка не е коректна");
-		}else{
-			$("#" + elemId + "_warning").removeClass("alert alert-warning").addClass("alert alert-success");
-			$("#" + elemId + "_warning").text("Оценката е коректна");
-		}
-	});
+    $(document).on("keyup", "#id_egn", function(){
+        var length = $(this).val().length;
+        var warningExists = document.getElementById("id_egn_warning");
+        var regexp = /(\d{10})/;
+        var match = regexp.exec($(this).val());
+        if (warningExists == null) {
 
-	$(document).on("click", ".required_field", function(){
-		var elemId = $(this).attr("id");
-		var warningExists = document.getElementById(elemId+"_warning");
-		if(warningExists == null) {
-			$("<div " 
-				+ "id=\""+ elemId +"_warning\"" 
-				+ "class=\"alert alert-warning form_warning\">"
-				+ "Това поле е задължително</div>").insertAfter(this);
-		}
-	});
+                $("<div "
+                    + "id=\"id_egn_warning\""
+                    + "class=\"alert alert-warning form_warning\">"
+                    + "</div>").insertAfter(this);
 
-	$(document).on("click", ".student_choices", function(){
-		var elemId = $(this).attr("id");
-		var warningExists = document.getElementById(elemId+"_warning");
-		if(warningExists == null){
-			$("<div " 
-				+ "id=\""+ elemId +"_warning\"" 
-				+ "class=\"alert alert-warning form_warning\">"
-				+ "Въведете СП или КМ</div>").insertAfter(this);	
-		}
-	});
+        }
+        if ((length < 10) || (match == null)) {
+            $("#id_egn_warning").removeClass("alert alert-success").addClass("alert alert-warning");
+            $("#id_egn_warning").text("ЕГН-то е твърде кратко или с неправилен формат");
+        }else{
+            $("#id_egn_warning").removeClass("alert alert-warning").addClass("alert alert-success");
+            $("#id_egn_warning").text("ЕГН-то е коректно");
+        }
+    });
 
-	/* CREATE CAMPAIGN FORM */
+    $(document).on("keyup", ".student_grade_input", function(){
+        var gradeWritten = parseInt($(this).val()) || false;
+        var elemId = $(this).attr("id");
+        var warningExists = document.getElementById(elemId+"_warning");
+        if (warningExists == null) {
 
-	var createCampaignButtonExists = document.getElementById("create_campaign_submit");
+                $("<div "
+                    + "id=\""+ elemId +"_warning\""
+                    + "class=\"alert alert-warning form_warning\">"
+                    + "</div>").insertAfter(this);
 
-	if (createCampaignButtonExists != null){
-		createCampaignButtonExists.disabled = true;
+        }
+        if ((gradeWritten == false) || (gradeWritten < 2) || (gradeWritten > 6)) {
+            $("#" + elemId + "_warning").removeClass("alert alert-success").addClass("alert alert-warning");
+            $("#" + elemId + "_warning").text("Въведената оценка не е коректна");
+        }else{
+            $("#" + elemId + "_warning").removeClass("alert alert-warning").addClass("alert alert-success");
+            $("#" + elemId + "_warning").text("Оценката е коректна");
+        }
+    });
 
-		setInterval(
-			function(){
-				var titleLength = $("#id_title").val().length;
-				var descriptionLength = $("#id_description").val().length;
+    $(document).on("click", ".required_field", function(){
+        var elemId = $(this).attr("id");
+        var warningExists = document.getElementById(elemId+"_warning");
+        if(warningExists == null) {
+            $("<div "
+                + "id=\""+ elemId +"_warning\""
+                + "class=\"alert alert-warning form_warning\">"
+                + "Това поле е задължително</div>").insertAfter(this);
+        }
+    });
 
-				if((titleLength > 0) && (descriptionLength > 0)){
-					createCampaignButtonExists.disabled = false;
-				}else{
-					createCampaignButtonExists.disabled = true;
-				}
-			},
-			3000
-		);
-	};
+    $(document).on("click", ".student_choices", function(){
+        var elemId = $(this).attr("id");
+        var warningExists = document.getElementById(elemId+"_warning");
+        if(warningExists == null){
+            $("<div "
+                + "id=\""+ elemId +"_warning\""
+                + "class=\"alert alert-warning form_warning\">"
+                + "Въведете СП или КМ</div>").insertAfter(this);
+        }
+    });
 
-	$('#edit_student_next').on('click', function (e) {
-		$('#create_student_form').find('[name="next"]').val('true');
-	});
+    /* CREATE CAMPAIGN FORM */
+
+    var createCampaignButtonExists = document.getElementById("create_campaign_submit");
+
+    if (createCampaignButtonExists != null){
+        createCampaignButtonExists.disabled = true;
+
+        setInterval(
+            function(){
+                var titleLength = $("#id_title").val().length;
+                var descriptionLength = $("#id_description").val().length;
+
+                if((titleLength > 0) && (descriptionLength > 0)){
+                    createCampaignButtonExists.disabled = false;
+                }else{
+                    createCampaignButtonExists.disabled = true;
+                }
+            },
+            3000
+        );
+    };
+
+    $('#edit_student_next').on('click', function (e) {
+        $('#create_student_form').find('[name="next"]').val('true');
+    });
 
 });
